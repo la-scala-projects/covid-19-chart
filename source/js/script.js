5bd3663be82adc52726ae645b6501142d2c0b525
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2020-03-30 01:28:11 +0200

    Add basic styles

--- source/js/script.js
+++ source/js/script.js
@@ -3,29 +3,29 @@
 const ctx = document.querySelector("#myChart");
 const dataLines = [
   {
-    label: "Cases",
-    data: [12, 19, 30, 35, 40, 100],
+    label: "Confirmed",
+    data: [12, 19, 30, 35, 40, 100, 300],
     fill: false,
     backgroundColor: "rgba(255, 0, 0, 1)",
     borderColor: "rgba(255, 0, 0, 0.5)"
   },
   {
     label: "Deaths",
-    data: [1, 2, 3, 4, 10, 30],
+    data: [1, 2, 3, 4, 10, 30, 40],
     fill: false,
     backgroundColor: "rgba(0, 0, 0, 1)",
     borderColor: "rgba(0, 0, 0, 0.5)"
   },
   {
-    label: "Hospital",
-    data: [3, 5, 7, 10, 20, 50],
+    label: "Percent",
+    data: [3, 5, 7, 10, 20, 50, 90],
     fill: false,
-    backgroundColor: "rgba(51, 199, 255, 1)",
-    borderColor: "rgba(51, 199, 255, 0.5)"
+    backgroundColor: "rgba(255, 131, 0, 1)",
+    borderColor: "rgba(255, 131, 0, 0.5)"
   },
   {
     label: "Recovered",
-    data: [0, 0, 0, 5, 10, 12],
+    data: [0, 0, 0, 5, 10, 12, 25],
     fill: false,
     backgroundColor: "rgba(34, 182, 47, 1)",
     borderColor: "rgba(34, 182, 47, 0.5)"
@@ -33,6 +33,7 @@ const dataLines = [
 ];
 
 const dataLabels = [
+  "March 23, 2020",
   "March 24, 2020",
   "March 25, 2020",
   "March 26, 2020",
