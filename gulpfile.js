625aa1a47fa6654a73292a43337405891f5a8247
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2020-03-29 18:47:16 +0200

    Integrate chart.js library in the project

--- gulpfile.js
+++ gulpfile.js
@@ -56,7 +56,8 @@ gulp.task("copy", function() {
         "source/fonts/**/*.{woff,woff2}",
         "source/img/**",
         "source/js/**",
-        "source/*.ico"
+        "source/*.ico",
+        "source/vendors/**"
       ],
       {
         base: "source"
